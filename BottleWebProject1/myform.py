8ddfe0d5435823b92c7d986fde07fc5ad98d6070
EgorSad06 <143409348+EgorSad06@users.noreply.github.com>
2026-03-27 00:20:06 +0300

    Add pdb control

--- BottleWebProject1/myform.py
+++ BottleWebProject1/myform.py
@@ -1,12 +1,18 @@
 from bottle import post, request, re
 from datetime import datetime
+import pdb
+
+
+user_data = {}
 
 @post('/home', method='post')
 def my_form():
-
     mail = request.forms.get('ADRESS')
     username = request.forms.get('USERNAME')
-    Question = request.forms.get('QUEST')
+    question = request.forms.get('QUEST')
+    
+
+    pdb.set_trace()
     
     if not mail or not username:
         return "Error: incorrect form"
@@ -15,12 +21,16 @@ def my_form():
     if not re.match(email_pattern, mail):
         return "Error: incorrect mail"
     
-
     username_pattern = r'^[A-Za-z]{4,}$'
     if not re.match(username_pattern, username):
         return "Error: username must be at least 4 characters long and contain only English letters"
     
+    user_data[mail] = question
+    
+    print(f"DEBUG - Dictionary contents: {user_data}")
+    print(f"DEBUG - Added entry: {mail} -> {question}")
+    
     current_date = datetime.now().strftime("%Y-%m-%d")
-    result_message = f"Thanks, {username}! The answer'{Question}' will be sent to the mail {mail}. Access Date: {current_date}"
+    result_message = f"Thanks, {username}! The answer '{question}' will be sent to the mail {mail}. Access Date: {current_date}"
     
     return result_message
